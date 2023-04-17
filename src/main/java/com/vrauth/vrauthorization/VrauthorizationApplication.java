f20b67d4d1d13dabf05ae02f25734b8273ee44e4
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-04-17 16:17:03 -0300

    chore: initial project

---
+++ src/main/java/com/vrauth/vrauthorization/VrauthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.vrauth.vrauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrauthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrauthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrauth/vrauthorization/VrauthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrauth.vrauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrauthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
